d0d1e341ad942cf0a4d549bbc959781ab3be4bb7
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-19 16:48:49 -0400

    Added the word Column to the attribute for the column name in the table produce_item in the file database_setup.py file.

--- vagrant/produce/database_setup.py
+++ vagrant/produce/database_setup.py
@@ -19,7 +19,7 @@ class ProduceItem(Base):
 
 	__tablename__ = 'produce_item'
 	
-	name = (String(80), nullable = False)
+	name = Column(String(80), nullable = False)
 	id = Column(Integer, primary_key = True)
 	description = Column(String(250))
 	price = Column(String(8))
