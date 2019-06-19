7c7cc02626b041336a5f720c457ab9f3537751ed
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-19 15:52:08 -0400

    This commit added the import for create_engine to the file database_setup.py in the produce folder.

--- vagrant/produce/database_setup.py
+++ vagrant/produce/database_setup.py
@@ -3,3 +3,4 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String 
 form sqalalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
