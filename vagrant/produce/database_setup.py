a9554b26d02da52a400e77fc1a8c26e99263936b
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-19 16:43:55 -0400

    Changed the word sqalalchemy on line four in the file database_setup.py file to sqlalchemy.

--- vagrant/produce/database_setup.py
+++ vagrant/produce/database_setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String 
-form sqalalchemy.ext.declarative import declarative_base
+form sqlalchemy.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
