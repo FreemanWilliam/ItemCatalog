5462ec2f3d9453079ded8ad55c8dad4abd84cabb
FreemanWilliam <myhousemyrules69@yahoo.com>
2019-06-19 15:53:31 -0400

    This commit created an instance of the declarative_base called Base in the file database_setup.py in the produce folder.

--- vagrant/produce/database_setup.py
+++ vagrant/produce/database_setup.py
@@ -4,3 +4,5 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 form sqalalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
+Base = declarative_base()
